d227961e8d44fcb4b4a4c53719e2379b0eb13756
dandicahyo <dandicahyo07@gmail.com>
2020-02-10 18:44:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidlifecycle/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.androidlifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // TODO: tambahkan callback onStart() di sini
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Application On Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Application On Stop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Application On Restart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Application On Resume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Application On Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Application On Destroy", Toast.LENGTH_SHORT).show();
+    }
+}
